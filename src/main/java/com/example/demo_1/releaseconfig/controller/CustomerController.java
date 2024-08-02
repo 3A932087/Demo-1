600bf4cce59fd80d34cf4c35fa9a66e3a8fd1c6a
la_lu_al <kelly901021@gmail.com>
2024-08-02 17:21:21 +0800

    衝突演示-local

--- src/main/java/com/example/demo_1/releaseconfig/controller/CustomerController.java
+++ src/main/java/com/example/demo_1/releaseconfig/controller/CustomerController.java
@@ -9,6 +9,7 @@ public class CustomerController {
     // local chanage
     // github
     // 2024.08.02 05:14
+    // 衝突演示-local
     @GetMapping
     public String getAllCustomer(){
         return "getAllUsers";
