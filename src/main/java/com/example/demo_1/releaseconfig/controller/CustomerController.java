8c9dad30608acbeeea52bd9886778ec153a321a1
la_lu_al <kelly901021@gmail.com>
2024-08-02 11:55:14 +0800

    local change

--- src/main/java/com/example/demo_1/releaseconfig/controller/CustomerController.java
+++ src/main/java/com/example/demo_1/releaseconfig/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
+    // local chanage
     @GetMapping
     public String getAllCustomer(){
         return "getAllUsers";
