ab0ac5413efd35c9e9a15b505c730e1608fc9ffb
la_lu_al <114425659+3A932087@users.noreply.github.com>
2024-08-04 22:53:24 +0800

    衝突測試-origin

--- src/main/java/com/example/demo_1/releaseconfig/controller/CustomerController.java
+++ src/main/java/com/example/demo_1/releaseconfig/controller/CustomerController.java
@@ -12,6 +12,7 @@ public class CustomerController {
     // 衝突演示-local
     // 衝突演示
     // 衝突測試-local
+    // 衝突測試-origin
     @GetMapping
     public String getAllCustomer(){
         return "getAllUsers";
